453eb619bbbba60a3bf94e1dee6b44bc0ae1f04e
Carter Carlson <pegahcarter@gmail.com>
2025-12-19 14:22:32 -0800

    fix: freeze functionality

--- src/contracts/ethereum/frxUSD/versioning/FrxUSD2.sol
+++ src/contracts/ethereum/frxUSD/versioning/FrxUSD2.sol
@@ -110,6 +110,18 @@ contract FrxUSD2 is ERC20Permit, ERC20Burnable, Ownable2Step {
         emit MinterRemoved(minter_address);
     }
 
+    function addFreezer(address _freezer) external onlyOwner {
+        if (isFreezer[_freezer]) revert AlreadyFreezer();
+        isFreezer[_freezer] = true;
+        emit AddFreezer(_freezer);
+    }
+
+    function removeFreezer(address _freezer) external onlyOwner {
+        if (!isFreezer[_freezer]) revert NotFreezer();
+        isFreezer[_freezer] = false;
+        emit RemoveFreezer(_freezer);
+    }
+
     /// @notice External admin gated function to unfreeze a set of accounts
     /// @param _owners Array of accounts to be unfrozen
     function thawMany(address[] memory _owners) external onlyOwner {
@@ -127,7 +139,8 @@ contract FrxUSD2 is ERC20Permit, ERC20Burnable, Ownable2Step {
 
     /// @notice External admin gated function to batch freeze a set of accounts
     /// @param _owners Array of accounts to be frozen
-    function freezeMany(address[] memory _owners) external onlyOwner {
+    function freezeMany(address[] memory _owners) external {
+        if (!isFreezer[msg.sender] && msg.sender != owner()) revert NotFreezer();
         uint256 len = _owners.length;
         for (uint256 i; i < len; ++i) {
             _freeze(_owners[i]);
@@ -135,8 +148,10 @@ contract FrxUSD2 is ERC20Permit, ERC20Burnable, Ownable2Step {
     }
 
     /// @notice External admin gated function to freeze a given account
-    /// @param _owner The account to be
-    function freeze(address _owner) external onlyOwner {
+    /// @param _owner The account to be frozen
+    function freeze(address _owner) external {
+        if (!isFreezer[msg.sender] && msg.sender != owner()) revert NotFreezer();
+
         _freeze(_owner);
     }
 
@@ -252,9 +267,19 @@ contract FrxUSD2 is ERC20Permit, ERC20Burnable, Ownable2Step {
     /// @param account The account being thawed
     event AccountThawed(address account);
 
+    /// @notice Event Emitted when an address is added as a freezer
+    /// @param account The account being added as a freezer
+    event AddFreezer(address account);
+
+    /// @notice Event Emitted when an address is removed as a freezer
+    /// @param account The account being removed as a freezer
+    event RemoveFreezer(address account);
+
     /* ========== ERRORS ========== */
     error ArrayMisMatch();
     error IsPaused();
     error IsFrozen();
+    error NotFreezer();
+    error AlreadyFreezer();
     error OwnerCannotInitToZeroAddress();
 }
