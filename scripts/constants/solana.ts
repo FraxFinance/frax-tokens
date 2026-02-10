a7172548003aeeefa3245f0dff36647c64067d65
dhruvinparikh <imsilent93@gmail.com>
2026-02-10 02:09:33 +0000

    feat: add LZ infra, chain IDs, hops, monad, stable constants for frax-lz-mesh

--- scripts/constants/solana.ts
+++ scripts/constants/solana.ts
@@ -1,13 +1,49 @@
 // LayerZero
+export const OFT_PROGRAM_ID = "E1ht9dUh1ZkgWWRRPCuN3kExEoF2FXiyADXeN3XyMHaQ";
 export const FPIOFT = "0xd3cee058686107cc51844f331ee213a33142ab299b5ce473c1cf3a8ddaa721a0";
 export const FRXETHOFT = "0x38dd9e11bbf63835dc61d3cbf259f4221f5987ac92982c96609b99634662dfb3";
 export const FRXUSDOFT = "0x5e208a73d5bb1c78e9dbf53badd7299afd6bee9acacdcd4fd668833e53c538ad";
 export const SFRXETHOFT = "0xbf2f1fc27286a43f25b05bd843a74a5478c4246343fa90c1fcb641a1caf46c61";
 export const SFRXUSDOFT = "0x8602f005ca65b6da46a3c6ce66ecd1d15be911ca650d5f418d369df184b584cf";
 export const WFRAXOFT = "0x4939035f8dd13d15a9386e28b6705519aa6f488791323466a3c0116a201e51aa";
-export const FPI = "8xKX8CRH9LxriRUNCPittu1jiovyQQr4EonWQjHZjWyH"
-export const FRXETH = "5sDrwVNiHMM2jC78hRBH1CtysDQYiNKihubgW2zNu8tf"
-export const FRXUSD = "GzX1ireZDU865FiMaKrdVB1H6AE8LAqWYCg6chrMrfBw"
-export const SFRXETH = "58zpC9acE6F4FBtd88L64NoWHJcmzLsQSy5bjz35Ydgv"
-export const SFRXUSD = "DUvWQMyASSkLNJFwsMDA4kwxEvmfaqpPGrvUVKtitX45"
-export const WFRAX = "zZbQjiRg8uSxZaPu996XuviuZeSY6nsaMuutKZQBJga"
+
+// Token Mints
+export const FPI = "8xKX8CRH9LxriRUNCPittu1jiovyQQr4EonWQjHZjWyH";
+export const FRXETH = "5sDrwVNiHMM2jC78hRBH1CtysDQYiNKihubgW2zNu8tf";
+export const FRXUSD = "GzX1ireZDU865FiMaKrdVB1H6AE8LAqWYCg6chrMrfBw";
+export const SFRXETH = "58zpC9acE6F4FBtd88L64NoWHJcmzLsQSy5bjz35Ydgv";
+export const SFRXUSD = "DUvWQMyASSkLNJFwsMDA4kwxEvmfaqpPGrvUVKtitX45";
+export const WFRAX = "zZbQjiRg8uSxZaPu996XuviuZeSY6nsaMuutKZQBJga";
+
+// Mint Authorities
+export const FPI_MINT_AUTHORITY = "F3YLUzqvJndUpypxUgxsFZQFHD3KUNC5QrkpNJKLBGje";
+export const FRXETH_MINT_AUTHORITY = "Ea6GvxUc4xh4LoPgJXePhKFsUcuW8G2RASdNYyuoohxr";
+export const FRXUSD_MINT_AUTHORITY = "52e8UiVqx28VEqyiEqMbMVehFC3LheNGqnA9m82XF6GK";
+export const SFRXETH_MINT_AUTHORITY = "BVQcQygw2f6nrDu6HNTfQ1ECjRRx8nm2CnBANt4yqqvp";
+export const SFRXUSD_MINT_AUTHORITY = "EK2qrg4ggSVmxNDU3qSvVcKx7HYpmNT3kWbLHMtL5vaV";
+export const WFRAX_MINT_AUTHORITY = "CMCA6bzoCH4KcDZtwCviFYF2c5tasLHjEhX8ThMeCpqk";
+
+// Escrows
+export const FPI_ESCROW = "EPmy89v6at8rxrV2n1E3xZP74MzaGi7BWdFNiA2xW64Z";
+export const FRXETH_ESCROW = "H17Anco2cnEtSyfSwt4XKhsU7nbFyP57qqXhsff39UW3";
+export const FRXUSD_ESCROW = "84AFSH3TSzyjbEFJX9z8sjpV7npTWq7f8ZR5zkLG22hX";
+export const SFRXETH_ESCROW = "AmjMhAYdCz2crdKYG4U9WMPT7ccHsXSx12qJ5TTjoHYq";
+export const SFRXUSD_ESCROW = "8vRGMiktFX3VhQRfwkaWsAG3KHHzVnRYEKkDKEYzgmCa";
+export const WFRAX_ESCROW = "FwmRDpyFBLkdk7BLRkWF5p5DqwBX2LzVTARGkG5haHbV";
+
+// OFT Stores
+export const FPI_OFT_STORE = "FFozEKoFQ1CZD6Kn7bpwAxaWhK1jEA76pjucvBBHf9ZH";
+export const FRXETH_OFT_STORE = "4pyqBQFhzsuL7ED76x3AyzT4bCVpMpQWXhS1LqEsfQtz";
+export const FRXUSD_OFT_STORE = "7LS6y37WXXCyBHkBU6zVpiqaqbkXLr4P85ZhQi3eonSp";
+export const SFRXETH_OFT_STORE = "DsJYjDF5yVSopMC15q9W42v833MhWGhCxcU2J39oS3wN";
+export const SFRXUSD_OFT_STORE = "A28EK6j1euK4e6taP1KLFpGEoR1mDpXR4vtfiyCE1Nxv";
+export const WFRAX_OFT_STORE = "5vqBiG7nxNnoCst8mEVVS6ax7C1ypEEenPfcZ4kLgj9B";
+// LayerZeroInfra
+export const LZ_ENDPOINT = "76y77prsiCMvXMjuoZ5VRrhG5qYBrUMYTE5WgHqgjEn6";
+export const LZ_RECEIVE_LIB_302 = "7a4WjyR8VZ7yZz5XJAKm39BUGn5iT9CKcv2pmG9tdXVH";
+export const LZ_SEND_LIB_302 = "7a4WjyR8VZ7yZz5XJAKm39BUGn5iT9CKcv2pmG9tdXVH";
+export const LZ_BLOCK_SEND_LIB = "2XrYqmhBMPJgDsb4SVbjV1PnJBprurd5bzRCkHwiFCJB";
+
+// Chain IDs
+export const LZ_CHAIN_ID = 30168;
+export const CHAIN_ID = 0;
